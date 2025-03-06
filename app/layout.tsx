c00509c4b2af58d09d41c947eb4a474dc659a82d
NoureddineElmhassani <noureddineelmhassani@gmail.com>
2025-03-06 20:05:16 +0100

    improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Century 21 | Real Estate",
@@ -8,21 +8,16 @@ export const metadata: Metadata = {
   icons: {
     icon: "/C21 logo rbz.png",
   },
-    generator: 'v0.dev'
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
-
-
-
-import './globals.css'
